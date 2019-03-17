edc6a762d1dbfc16f8d7b2cfc3b926549595c0bb
kangyul <39001942+kangyul@users.noreply.github.com>
2019-03-17 22:03:30 +0900

    made first project

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() 
+{
+	
+	printf("TTTTTTTTTT \n");
+	printf("TTTTTTTTTT \n");
+	printf("%8s", "TT \n");
+	printf("%8s", "TT \n");
+	printf("%8s", "TT \n");
+	
+			
+	return 0;
+}
